1e8c96878b325fa5eea5fd755870fe74330c94e1
Alexey <alexei.samara63@gmail.com>
2022-12-15 16:40:57 +0400

    Update dolnikov.js
    новый сайт

--- cypress/integration/1-My-tests/dolnikov.js
+++ cypress/integration/1-My-tests/dolnikov.js
@@ -1,9 +1,9 @@
 
-describe('Тестирование dolnikov.ru', function () {
+describe('Тестирование сайта Математический Факультет', function () {
 
     it('Проверка, что при переходе в статью урл будет верный', function () {
-        cy.visit('https://dolnikov.ru/blog/');
-        cy.contains('Про образование').click();
-        cy.url().should('eq', 'https://dolnikov.ru/blog/all/pro-obrazovanie/')
+        cy.visit('https://math.uniyar.ac.ru/professors/show/2f195ecf-4ca7-442d-b589-293060d03108');
+        cy.contains('Студентам').click();
+        cy.url().should('eq', 'https://math.uniyar.ac.ru/for-students')
     })
 })
