bc93cd63cf6b0bb9132705f8c10e4fbd7625a262
Alexey <alexei.samara63@gmail.com>
2022-12-15 17:24:28 +0400

    2
    go

--- cypress/integration/1-My-tests/firstTest.js
+++ cypress/integration/1-My-tests/firstTest.js
@@ -2,7 +2,7 @@
 describe('Тестирование сайта Математический Факультет', function () {
 
     it('Проверка, что при переходе в статью урл будет верный', function () {
-        cy.visit('https://math.uniyar.ac.ru/professors/show/2f195ecf-4ca7-442d-b589-293060d03108');
+        cy.visit('https://math.uniyar.ac.ru');
         cy.contains('Студентам').click();
         cy.url().should('eq', 'https://math.uniyar.ac.ru/for-students')
     })
